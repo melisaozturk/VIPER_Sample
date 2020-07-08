892c6372b66a865111502206096d2173fd53f686
Melisa Öztürk <mlsaoztrk@icloud.com>
2020-07-08 07:48:16 +0300

    detail init

---
+++ TrendyolInterviewProject/Module/DetailProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  Protocols.swift
+//  TrendyolInterviewProject
+//
+//  Created by melisa öztürk on 8.07.2020.
+//  Copyright © 2020 melisa öztürk. All rights reserved.
+//
+
+import Foundation
